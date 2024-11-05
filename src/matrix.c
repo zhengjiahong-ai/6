85f2243905c24b222a4f77aa6910f3e1221eb220
zhengjiahong-ai <241880355@smail.nju.edu.cn>
2024-11-06 06:54:27 +0800

    change

--- src/matrix.c
+++ src/matrix.c
@@ -1,4 +1,11 @@
 #include "matrix.h"
 void matrix_multiply(int A[][MAX_SIZE],int B[][MAX_SIZE], int C[][MAX_SIZE],  int m, int n, int p){
-    //TODO    
+    for(int i=0;i<m;i++){
+        for(int j=0;j<p;j++){
+            C[i][j]=0;
+            for(int k=0;k<n;k++){
+                C[i][j]+=A[i][k]*B[k][j];
+            }
+        }
+    }
 }
